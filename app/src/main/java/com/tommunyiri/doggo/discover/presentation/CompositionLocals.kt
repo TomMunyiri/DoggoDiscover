b12fc794832eab453966e060f50bdfa294462f86
Thomas Munyiri <tmmunyiri@gmail.com>
2025-05-16 22:31:26 +0300

    network models setup

---
+++ app/src/main/java/com/tommunyiri/doggo/discover/presentation/CompositionLocals.kt
@@ -0,0 +1,7 @@
+package com.tommunyiri.doggo.discover.presentation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.dp
+
+val LocalMainContentPadding = compositionLocalOf { PaddingValues(0.dp) }
